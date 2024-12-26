c021e6f4a634cfa8bc84ad8103e09e429894ab7b
Takumi Ikeda <tkik13201@gmail.com>
2024-12-27 03:47:04 +0900

    fix: fixed maxMemoryUsage unit from KB to KiB (revert)

--- maven-root/mtsa/src/main/java/ltsa/lts/result/LTSResult.java
+++ maven-root/mtsa/src/main/java/ltsa/lts/result/LTSResult.java
@@ -34,7 +34,7 @@ public class LTSResult {
     private ZonedDateTime finishedAt;
     @SerializedName("duration [ms]")
     private Duration duration;
-    @SerializedName("maxMemoryUsage [KB]")
+    @SerializedName("maxMemoryUsage [KiB]")
     private Long maxMemoryUsage;
 
     public LTSResult(String mode, String command, String ltsFilePath, String target) {
